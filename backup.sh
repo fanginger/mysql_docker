df3f1dbf8963e53f44ddc75b0b79101eb5f63936
tzuying <tzuying0312@gmail.com>
2020-12-22 13:25:45 +0800

    change .sql path

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 mysqldump --databases testdb \
-  --user=root --password='ginger123'\
-  > ./all-databases.sql
+  --user=root --password='ginger123' >> /mysql_docker/backup/all-databases.sql
   # > /home/${USER}/mysql_backup:/mysql_docker/backup/all-databases.sql
     # > /mysql_docker/backup/all-databases.sql
--- drun.sh
+++ drun.sh
@@ -3,5 +3,7 @@ IMAGE_NAME=gingerfan/wpg
 docker run  -it --rm  \
      -p 80:8888 --name=${CONTAINER} \
      -e MYSQL_ROOT_PASSWORD=ginger123 \
-     -v /home/${USER}/mysql_backup:/mysql_docker/backup \
+     -v /home/dbbackup:/mysql_docker/backup\
      ${IMAGE_NAME} 
+
+# -v /Users/tzuying/Desktop/mysql_docker/local_backup:/mysql_docker/backup\
