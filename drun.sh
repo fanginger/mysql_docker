57427b4c9ab6bdec7b82bda5d643ab6108aed520
tzuying <tzuying0312@gmail.com>
2020-12-23 14:38:54 +0800

    12/23

--- drun.sh
+++ drun.sh
@@ -2,13 +2,9 @@ CONTAINER=wpg
 IMAGE_NAME=gingerfan/wpg
 docker run  -it --rm  \
      -p 80:8888 --name=${CONTAINER} \
-     -v /Users/ginger/Desktop/mysql_docker/local_backup:/mysql_docker/backup\
+     -v /Users/ginger/Desktop/mysql_docker/dbbackup:/mysql_docker/backup\
      -e password='testpass'\
      ${IMAGE_NAME} 
 
-     #      -e user='ddt'\
-     # -e password='yang'\
-     
-
-# -v /Users/tzuying/Desktop/mysql_docker/local_backup:/mysql_docker/backup\
+# -v /Users/tzuying/Desktop/mysql_docker/dbbackup:/mysql_docker/backup\
 # -v /home/dbbackup:/mysql_docker/backup\
