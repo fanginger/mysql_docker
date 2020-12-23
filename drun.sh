9630237886232a838c8aeaeb2e8c4e6bcb348cad
tzuying <tzuying0312@gmail.com>
2020-12-23 14:37:30 +0800

    12/23

--- drun.sh
+++ drun.sh
@@ -3,7 +3,6 @@ IMAGE_NAME=gingerfan/wpg
 docker run  -it --rm  \
      -p 80:8888 --name=${CONTAINER} \
      -v /Users/ginger/Desktop/mysql_docker/local_backup:/mysql_docker/backup\
-     -e user='ddt'\
      -e password='testpass'\
      ${IMAGE_NAME} 
 
--- load_container.sh
+++ load_container.sh
@@ -12,11 +12,11 @@ until mysqladmin ping >/dev/null 2>&1; do
   echo -n "."; sleep 0.2
 done
 mysql --user=root  --execute="CREATE DATABASE testdb;"
-mysql --user=root  --execute="CREATE USER '$user'@'%' IDENTIFIED BY '$password';"
-mysql --user=root  --execute="GRANT ALL ON testdb.* TO '$user'@'%';"
+mysql --user=root  --execute="CREATE USER 'ddt'@'%' IDENTIFIED BY '$password';"
+mysql --user=root  --execute="GRANT ALL ON testdb.* TO 'ddt'@'%';"
 mysql --user=root   <"intit.sql"
 
 service mysql start &&tail -f /var/log/mysql/error.log
-# python apiServer.py $password $user
+# python apiServer.py $password 
 
 # bash
