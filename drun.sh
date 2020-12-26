de37ce23a1a40f1c0fc48aac90d32aed8a847cce
ginger <gingerfanfan@gmail.com>
2020-12-26 16:45:15 +0800

    crontab fix

--- drun.sh
+++ drun.sh
@@ -2,7 +2,7 @@ CONTAINER=wpg
 IMAGE_NAME=gingerfan/wpg
 docker run  -it --rm  \
      -p 80:8888 --name=${CONTAINER} \
-     -v /Users/ginger/Desktop/mysql_docker/dbbackup:/mysql_docker/backup\
+     -v /home/dbbackup:/mysql_docker/backup\
      -e password='testpass'\
      ${IMAGE_NAME} 
 
